cbbb2bc1e62ecd68831537d371001fa57af5a675
Damien Brockmann <damien.brockmann@macmillan.com>
2018-11-16 09:01:23 -0500

    savi test

--- features/steps/SAVI/brightcoveplayer.js
+++ features/steps/SAVI/brightcoveplayer.js
@@ -9,24 +9,24 @@ const login = loadConfig('login');
 const assert = require('assert');
 
 const saviVerification1 = new PageObject('savi-verification1.json', stepsPath);
-const loginPage = new PageObject('saplinglearning.json', stepsPath);
+const loginPage = new PageObject('saplinglearning-login.json', stepsPath);
 
 Given('I login to sapling SAVIPO2', async function () {
   const url = config.loginURL;
   log.debug(`Loading URL ${url}`);
   await getDriver().get(url);
-  await sleep(5000);
+  // await sleep(5000);
   await loginPage.populate('input_username', login.username);
   await loginPage.populate('input_password', login.password);
   await loginPage.populate('login', 'click');
-  await sleep(5000);
+  // await sleep(5000);
 });
 
 When('I open the Brightcove Player', async function () {
   const url = config.saviVerification;
   log.debug(`Loading URL ${url}`);
   await getDriver().get(url);
-  await sleep(5000);
+  // await sleep(5000);
   await saviVerification1.populate('open_brightcove_player', 'click');
 });
 
