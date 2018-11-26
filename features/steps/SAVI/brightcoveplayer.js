8db64e10cd50f2305cde345d44f166bdff74b693
Jeff Sims <jeff.sims@macmillan.com>
2018-11-26 17:02:50 -0500

    WIP: standalone test added, still working out bugs

--- features/steps/SAVI/brightcoveplayer.js
+++ features/steps/SAVI/brightcoveplayer.js
@@ -4,43 +4,58 @@ const stepsPath = process.cwd() + '/features/pageDefs/';
 const { PageObject } = require('../../../app/pageObject');
 const { log } = require('../../../app/logger');
 const { getDriver, sleep } = require('../../../app/driver');
+const { By } = require('selenium-webdriver');
 const config = loadConfig('config');
 const login = loadConfig('login');
 const assert = require('assert');
 
 const loginPage = new PageObject('saplinglearning-login.json', stepsPath);
 const saviVerification1 = new PageObject('savi-verification1.json', stepsPath);
+const saviBrightcoveVideo = new PageObject('savi-brightcove-video.json', stepsPath);
+const saviAssignment = new PageObject('savi-assignment.json', stepsPath);
 
 Given('I login to sapling SAVIPO2', async function () {
   const url = config.loginURL;
   log.debug(`Loading URL ${url}`);
   await getDriver().get(url);
-  // await sleep(5000);
   await loginPage.populate('input_username', login.username);
   await loginPage.populate('input_password', login.password);
   await loginPage.populate('login', 'click');
-  // await sleep(5000);
 });
 
 When('I open the Brightcove Player', async function () {
   const url = config.saviVerification;
   log.debug(`Loading URL ${url}`);
   await getDriver().get(url);
+  await sleep(500);
+  await saviVerification1.populate('open_brightcove_player', 'click');
+
+  await sleep(500);
+  await getDriver().navigate().refresh();
+  await sleep(500);
+  log.debug('Launching assignment preview');
   await saviVerification1.populate('open_assignment', 'click');
-  await sleep(5000);
-  // var elementIframe = await saviVerification1.getElement('ae_iframe');
-  // getDriver().switchTo().frame('aeIframe');
+});
+
+Given('I open the standalone link to a video', async function () {
+  const url = config.standalone;
+  log.debug(`Loading URL ${url}`);
+  await getDriver().get(url);
 });
 
 Then('I can play a video', async function () {
-  await sleep(5000);
-  await saviVerification1.populate('play_brightcove_player', 'click');
-  // const playButton = await saviVerification1.checkWebElementExists('play_brightcove_player');
-  // assert(playButton, 'The play button was not displayed.');
-  await sleep(5000);
+  await sleep(8000);
+
+  await saviAssignment.populate('big_play_button', 'click');
+  await sleep(2000);
+
+  // const currentTime = await saviBrightcoveVideo.getElementValue('video', 'currentTime');
+  // log.info(`currentTime: ${currentTime}`);
+  // console.log(currentTime);
+  // await sleep(500);
 });
 
-// AfterAll(function () {
-//   getDriver().quit();
-//   return Promise.resolve();
-// });
+AfterAll(function () {
+  getDriver().quit();
+  return Promise.resolve();
+});
--- features/tests/SAVI/brightcoveplayer.feature
+++ features/tests/SAVI/brightcoveplayer.feature
@@ -1,6 +1,10 @@
 Feature: Brightcove Video Player
 
-  Scenario: Verify that Brightcove Player is available in SAVI PO2 dot me
+  Scenario: Verify that Brightcove Player is available at SAVIPO2 dotme
     Given I login to sapling SAVIPO2
     When I open the Brightcove Player
     Then I can play a video
+
+  # Scenario: Verify that Brightcove Player is available at standalone link
+  #   Given I open the standalone link to a video
+  #   Then I can play a video
