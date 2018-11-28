cc91972532bd0a5b60615528ba2f66a8981755ed
Mennen <nick.mennen@mlmustxaus-0161.newyork.hbpub.net>
2018-11-28 10:01:28 -0600

    changes to driver

--- app/driver.js
+++ app/driver.js
@@ -1,103 +1,46 @@
-// ------------ Start up the cselenium webdriver ------------
 const webdriver = require('selenium-webdriver');
-const { log } = require('./logger');
-const { loadConfig } = require('./util');
-
+const config = require('../config.js');
+const process = require("process");
+const minimist = require("minimist");
 // https://stackoverflow.com/questions/49862078/protractor-and-cucumber-function-timed-out-using-async-await
-var {setDefaultTimeout} = require('cucumber');
+var { setDefaultTimeout } = require('cucumber');
+setDefaultTimeout(700 * 700);
 
-const config = loadConfig('config');
 
-// setDefaultTimeout(60 * 500);
-setDefaultTimeout(700 * 700); 
-let service;
 
-// Show Process config files
-process.argv.forEach(function (val, index, array) {
-  log.debug(index + ': ' + val);
-});
+const buildDriver = function() {
+  var builder;
+  var capabilities;
+  var argv = minimist(process.argv.slice(2));
 
-const buildDriver = function () {
-  const config = loadConfig('config');
-
-  const browser = config && config.driver && config.driver.type ? config.driver.type : 'chrome';
-  const headless = config && config.driver && config.driver.headless !== undefined ? config.driver.headless : true;
-  const screen = {
-    width: 640,
-    height: 480
+  var capabilities = {
+    "browserName": argv.browser || "chrome",
+    "takesScreenshot": true,
+    "version": argv.version
   };
-  const driver = new webdriver.Builder();
-  if (browser === 'chrome') {
-    driver.forBrowser(browser)
-      .withCapabilities(webdriver.Capabilities.chrome());
-    if (headless) {
-      driver.setChromeOptions(new chrome.Options().headless().windowSize(screen));
-    }
-    service = new chrome.ServiceBuilder(chromePath).build();
-    chrome.setDefaultService(service);
-  } else if (browser === 'firefox') {
-    driver.forBrowser(browser)
-      .withCapabilities(webdriver.Capabilities.firefox());
-    if (headless) {
-      driver.setFirefoxOptions(new firefox.Options().headless().windowSize(screen));
-    }
 
-    service = new firefox.ServiceBuilder(firefoxPath).build();
+  if (mode === 'local') {
+    builder = new WebDriver.Builder()
+      .withCapabilities(capabilities)
+      .build();
+  } else if (mode === 'browserstack') {
+    capabilities = _.merge(capabilities, config.browserstack);
+
+    builder = new WebDriver.Builder()
+      .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+      .withCapabilities(capabilities)
+      .build();
   } else {
-    throw new Error(`Driver not found for: ${browser}`)
+    builder = new WebDriver.Builder()
+      .usingServer("http://selenium:4444/wd/hub")
+      .withCapabilities(capabilities)
+      .build();
   }
-  return driver.build();
+
+  return builder;
 }
 
 
 module.exports = {
   build: build
 };
-
-
-/**
- * http://usejsdoc.org/
- */
-'use strict';
-const webdriver = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const { log } = require('./logger');
-
-var coreAutomation = function (worldData) {
-  let that = {};
-  that.config = require('../../../config/config.json');
-  log.debug('Configuration loaded');
-  that.loginAccounts = {};
-
-  log.debug('baseURL:' + that.config['baseURL']);
-
-  that.initChrome = function () {
-    var path = require('chromedriver').path;
-
-    var chromeService = new chrome.ServiceBuilder(path).build();
-    chrome.setDefaultService(chromeService);
-
-    var driver = new webdriver.Builder()
-      .withCapabilities(webdriver.Capabilities.chrome())
-      .build();
-  };
-
-  that.initDriver = function () {
-    switch (that.config.loaddriver) {
-      case 'chrome': {
-        that.initChrome;
-        break;
-      }
-      default: {
-        that.initChrome;
-      }
-    }
-  };
-  that.test = function () {
-    log.debug('Executing test() function in coreAutomation');
-  };
-
-  return that;
-};
-
-module.exports = coreAutomation;
