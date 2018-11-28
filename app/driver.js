4cf769ae88d8b0777ca2fc08cfd92689dd706525
Mennen <nick.mennen@mlmustxaus-0161.newyork.hbpub.net>
2018-11-28 11:17:45 -0600

    general cleanup of the driver file

--- app/driver.js
+++ app/driver.js
@@ -2,15 +2,10 @@ const webdriver = require('selenium-webdriver');
 const config = require('../config.js');
 const process = require("process");
 const minimist = require("minimist");
-// https://stackoverflow.com/questions/49862078/protractor-and-cucumber-function-timed-out-using-async-await
-var { setDefaultTimeout } = require('cucumber');
-setDefaultTimeout(700 * 700);
-
 
 
 const buildDriver = function() {
   var builder;
-  var capabilities;
   var argv = minimist(process.argv.slice(2));
 
   var capabilities = {
