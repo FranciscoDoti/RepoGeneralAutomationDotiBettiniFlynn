563a8980b418f2b54a7c2657525b02deeccdd5a1
Stuart Owens <wildporpoisegaming1@gmail.com>
2018-11-30 08:59:39 -0600

    refactors some of adm page Obj

--- page/a_master.js
+++ page/a_master.js
@@ -1,5 +1,7 @@
 page = {
-    login : require('./login.json')
+    login : require('./login.json'),
+    createAccount: require('./create-account.json'),
+    authAdm: require('./auth-admin.json')
 };
 
 module.exports = page;
