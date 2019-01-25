31bf9eab96c8cdcc81ba61afb9ecad209b4625b0
Greeshma Reddy <greeshma.reddy.contractor@macmillan.com>
2019-01-25 08:51:50 -0500

    changes in feature file

--- features/COURSE/step/read-and-practice.js
+++ features/COURSE/step/read-and-practice.js
@@ -5,7 +5,7 @@ const format = require('string-format');
 const expect = require('chai').expect;
 const _ = require('lodash');
 
-Then(/^I verify "(.*) sysytem "(.*)" data$/, async function (system, feature, data_table) {
+Then(/^I verify "(.*) system "(.*)" data$/, async function (system, feature, data_table) {
   let qa = new selenium(this.driver);
   for (let i = 0; i < data_table.rows().length; i++) {
     let PAGE = await _.get(page, [system, feature, data_table.hashes()[i].course_page]);
