adee39a7d8b9bb54a68a3bbf2a7f5199d4af3c82
Stuart Owens <wildporpoisegaming1@gmail.com>
2019-01-31 17:06:13 -0600

    validating the activities in the course

--- app/selenium.js
+++ app/selenium.js
@@ -33,29 +33,21 @@ module.exports = function (driver) {
             yield element.click();
           }
         }
-        // element_array.push(element);
       }
-      // return element_array;
     }),
 
-    elementExists: Promise.coroutine(function * (should_exist, element) {
-      var tick = Promise.coroutine(function * tick () {
-        var displayed = yield element.isDisplayed();
-        return (should_exist === displayed);
-      });
-      function poll () {
-        return tick().catch(function (e) {
-          switch (e.name) {
-            case 'NoSuchElementError':
-              return !should_exist;
-            case 'StaleElementReferenceError':
-              return false;
-            default:
-              throw e;
-          }
-        });
-      }
+    getTextOfElementInArray: Promise.coroutine(function * (selector, text) {
+      let locator = this._locator(selector);
+      yield this._exists(true, locator);
+      let elements = yield driver.findElements(locator);
 
+      for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
+        let elementText = yield element.getText();
+        if(text === elementText){
+          return elementText;
+        }
+      }
     }),
 
     goTo: Promise.coroutine(function * (url) {
