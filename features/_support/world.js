1cedaff7808e4f752165df44cc1016ea5c3f4de1
Lino Carnesecca <Lino.Carnesecca@macmillan.com>
2019-02-05 12:26:51 +0100

    - enable browser level logging - put docker file in right context

--- features/_support/world.js
+++ features/_support/world.js
@@ -1,6 +1,13 @@
 const {setWorldConstructor, setDefaultTimeout} = require('cucumber');
 const seleniumWebdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 const config = require('../../config.js');
+var options = new chrome.Options().headless();
+var prefs = new seleniumWebdriver.logging.Preferences();
+
+prefs.setLevel(seleniumWebdriver.logging.Type.BROWSER, seleniumWebdriver.logging.Level.ALL);
+options.setLoggingPrefs(prefs);
+
 require('chromedriver');
 
 function CustomWorld() {
@@ -8,6 +15,8 @@ function CustomWorld() {
 
   if (config.mode === 'local-achieve') {
     builder = new seleniumWebdriver.Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(options)
       .usingServer("http://selenium.local-mml.cloud:4444/wd/hub")
       .withCapabilities(config.capabilities)
       .build();
