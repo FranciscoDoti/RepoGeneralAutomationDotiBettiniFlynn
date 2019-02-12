a71d14e238df9d2ecf9a5fb24d21409bc42ba5f0
Steve Rutherford <steve.rutherford.contractor@macmillan.com>
2019-02-12 23:29:22 +0000

    Merged in srutherford-MTH-727 (pull request #38)
    MTH-727: Expression tests
    
    * Need to have them run
    * Working local
    * Remove yarn.lock
    * Clean up imports
    * Comments
    * Revert
    
    Approved-by: Jason Norment <jason.norment.contractor@macmillan.com>
    Approved-by: hope.miller <hope.miller@macmillan.com>

--- features/MATH/step/greq-expression.js
+++ features/MATH/step/greq-expression.js
@@ -4,17 +4,17 @@ const selenium = require('../../../app/selenium.js');
 const page = require('../../master-page.js');
 const { Key } = require('selenium-webdriver')
 
-When(/^I set the grade as Relation type and input "(.*)" equation$/, async function (eval) {
+When(/^I set the grade as Expression type and input "(.*)" equation$/, async function (eval) {
   let qa = new selenium(this.driver);
   let evalGrade = require(`../_data/${eval}.json`);
   await qa.exists(page.math.raptorAms.correctTab);
   await qa.click(page.math.raptorAms.correctTab);
-  await qa.input(page.math.raptorAms.gradeAs, 'Relation');
+  await qa.input(page.math.raptorAms.gradeAs, 'Expression');
   await qa.click(page.math.raptorAms.gradeAs);
   await qa.sendKeys(page.math.raptorAms.equationField, Key.RETURN)
   await qa.sendKeys(page.math.raptorAms.equationField, Key.BACK_SPACE)
-  await qa.executeScript(`const ta=document.querySelectorAll('textarea.ace_text-input'); ta[1].value='${evalGrade.EvalQ1.Equation}'; ta[1].dispatchEvent(new Event('input'))`);
+  await qa.executeScript(`const ta=document.querySelectorAll('textarea.ace_text-input'); ta[1].value='${evalGrade.EvalQ1.Expression}'; ta[1].dispatchEvent(new Event('input'))`);
   await qa.sendKeys(page.math.raptorAms.prefixField, Key.RETURN)
   await qa.sendKeys(page.math.raptorAms.prefixField, Key.BACK_SPACE)
-  await qa.executeScript(`const ta=document.querySelectorAll('textarea.ace_text-input'); ta[0].value='${evalGrade.EvalQ1.Equation}'; ta[0].dispatchEvent(new Event('input'))`);
+  await qa.executeScript(`const ta=document.querySelectorAll('textarea.ace_text-input'); ta[0].value='${evalGrade.EvalQ1.Expression}'; ta[0].dispatchEvent(new Event('input'))`);
 });
--- features/MATH/test/local-greq-relation.feature
+++ features/MATH/test/local-greq-relation.feature
@@ -1,17 +1,17 @@
 Feature: Creating a new AMS raptor item for eval type Relation
 
 Scenario: User creates a Relation type graded equation 2x+26=0 module and grades correct answer
-  
+
   Given I login to "local" login page as "gozer"
   When I am in the AMS page
   And I click on the New Raptor item
 
-  When I am on the AuthorApp item page 
+  When I am on the AuthorApp item page
   And I select Graded equation and save as "relationGrad"
   And I click on the Question tab, and add an Answer field
   And I set the grade as Relation type and input "math-eqn" equation
-  And I save the question  
-  
+  And I save the question
+
   When I am in Take Mode and input the correct equation "2x+26=0"
   And I simulate grading
   Then My answer is graded correctly
