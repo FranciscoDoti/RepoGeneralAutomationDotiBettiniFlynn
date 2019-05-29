b641767b1af6cd7eca2f25343b4ad57106b442d3
Thomas Dsilva <thomas.dsilva.contractor@macmillan.com>
2019-05-28 22:16:04 -0500

    added functionality for passing screenshot flags from command line, including true, false and onfail

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,8 +1,12 @@
 var { After, AfterAll } = require('cucumber');
-const { closeBrowser, resetBrowser } = require(`${process.cwd()}/app/driver`);
+const { closeBrowser, resetBrowser, takeScreenshot } = require(`${process.cwd()}/app/driver`);
 
 After(async function(scenario){
   console.log(scenario.result.status);
+  if (this.screenshots.toLowerCase().includes('onfail') 
+  && scenario.result.status.toLowerCase().includes('fail')) {
+    await this.attach(await takeScreenshot(), 'image/png');
+  }
   await resetBrowser();
 });
 
--- features/support/world.js
+++ features/support/world.js
@@ -1,12 +1,15 @@
 const { setWorldConstructor, setDefaultTimeout, setDefinitionFunctionWrapper } = require('cucumber');
 const { takeScreenshot } = require(`${process.cwd()}/app/driver`);
 const config = require(`${process.cwd()}/config/config.json`);
+const argv = require('minimist')(process.argv.slice(2));
 
 function ThisWorld({ attach }) {
-  this.environment = config.environment;
-  this.mode = config.executionMode;
-  this.browser = config.browser;
-  this.screenshots = config.screenshots;
+  this.environment = argv.env || config.environment;
+  this.mode = argv.mode || config.mode;
+  this.browser = argv.browser || config.browser;
+  this.screenshots = argv.screenshots || config.screenshots;
+  this.headless = argv.headless || config.headless;
+
   this.attach = attach;
   this.downloadLocation = process.cwd() + '/reports/downloads';
   setDefaultTimeout(2*config.timeout);
@@ -18,8 +21,9 @@ setDefinitionFunctionWrapper(function (fn) {
   return async function () {
     await fn.apply(this, arguments);
     try {
-      var screenshot = await takeScreenshot();
-      await this.attach(screenshot, 'image/png');
-    } catch(err){ };
+      if (this.screenshots.toLowerCase().includes("true")) {
+        await this.attach(await takeScreenshot(), 'image/png');
+      }
+    } catch (err) {};
   };
 });
