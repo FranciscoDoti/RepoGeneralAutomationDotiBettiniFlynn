aa88a91984402f1b8d85ff7250b471d33f736cef
kaushikbanerjee <kaushikbanerjee@QAIT.COM>
2019-05-31 01:00:36 +0530

    Refactored code as per Thomas Review

--- features/ASSESSMENT/steps/raptor.js
+++ features/ASSESSMENT/steps/raptor.js
@@ -3,35 +3,26 @@ const { When, Then}=require('cucumber');
 const pages = require(`${process.cwd()}/features/ASSESSMENT/pages/.page`).pages;
 const mathpages =require(`${process.cwd()}/features/MATH/pages/.page.js`).pages;
 var action = require(`${process.cwd()}/features/ASSESSMENT/util/actions`).Actions;
-const { resetBrowser } = require(`${process.cwd()}/app/driver`);
 const fs = require('fs');
-const expect = require('chai').expect;
 
-When(/^I added "(.*)" module from the Add module pallete$/, async function (moduleType) {
+When(/^I added "(.*)" module$/, async function (moduleType) {
+    await mathpages.ams.click('raptorNewItem');
+    await mathpages.raptorAms.switchToTab('Raptor Authoring');
+    await mathpages.raptorAms.assertElementExists('addMenuBar');
     await pages.raptor.click('addLink');
-    var pallete=await action().selectModulePallete(moduleType);
-    await pages.raptor.click('modulePallete',pallete);
+    await pages.raptor.click('modulePallete',moduleType);
 });
 
-When(/^I select the canvas to add the selected module$/, async function () {
+Then(/^I verify item has been created$/, async function () {
     await pages.raptor.click('contentArea');
-});
-
-When(/^I save the item as draft$/, async function () {
     let itemid = await mathpages.ams.getText('getItemid');
     await pages.raptor.click('moreButton');
     await pages.raptor.click('saveAsDraft');
 
-  // writing item id number into a file
-  let num = itemid.split(" / ")[0];
-  fs.writeFileSync('raptor-itemId.txt', num);
-});
-
-When(/^I navigate back to AMS landing page$/, async function () {
+    // writing item id number into a file
+    let num = itemid.split(" / ")[0];
+    fs.writeFileSync('raptor-itemId.txt', num);
     await mathpages.raptorAms.switchToTab('Sapling Learning');
-});
-
-Then(/^I verify item has been created$/, async function () {
     let savedItemId = fs.readFileSync("raptor-itemId.txt").toString();
     await pages.raptor.assertElementExists('amsItemCreate',savedItemId);
 });
--- features/ASSESSMENT/tests/cee-create-raptor.feature
+++ features/ASSESSMENT/tests/cee-create-raptor.feature
@@ -2,20 +2,6 @@ Feature: CEE module type creation
     Scenario: Verify that user can create CEE type of raptor item
 
     Given I login to AMS as "all-permissions-author"
-        When I click on the New Raptor item in the AMS page
-        And I navigate to AuthorApp tab
-        And I added "Chemical Equation" module from the Add module pallete
-        And I select the canvas to add the selected module
-        And I save the item as draft
-        And I navigate back to AMS landing page
-        Then I verify item has been created
-      # When I added "Chemical Equation" module from the Add module pallete
-    #    And I select the canvas to add the selected module 
-    #    And I try to save the item
-    #    Then I navigate back to AMS landing page
-    #    And I see the new raptor item in the items listing with following details
-    #    | coloumn_name| value|
-    #    | Author      | Raptor Admin |
-    #    | Status      | In Progress  |
-    #    | Module Type | CEE          |
+    When I added "Chemical Equation" module
+    Then I verify item has been created
 
