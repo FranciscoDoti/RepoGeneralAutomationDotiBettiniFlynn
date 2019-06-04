9f52b8c7cc4d7edc8edd1682b8b343654e538f75
Jose Mohamed <jmohamed@sparkdigital.com>
2019-06-04 18:40:16 +0000

    Merged in md-automation (pull request #87)
    MD automation
    
    * Basic grading tests
    
    * Removed unnecessary line
    
    * Removed unnecessary code
    
    * Undo unwanted changes
    
    * Undo unwanted changes
    
    * Undo unwanted changes
    
    * Undo unwanted changes
    
    * Removed code
    
    * Removed unneeded file
    
    * Comments from PR
    
    Approved-by: Thomas Dsilva <thomas.dsilva.contractor@macmillan.com>
    Approved-by: Kaushik Banerjee <kaushik.banerjee.contractor@macmillan.com>
    Approved-by: Prakhar Kumar Tiwari <prakhar.kumartiwari.contractor@macmillan.com>

--- features/ASSESSMENT/pages/.page.js
+++ features/ASSESSMENT/pages/.page.js
@@ -2,6 +2,8 @@ const stepsPath = `${process.cwd()}/features/ASSESSMENT/pages/`;
 const { PageObject } = require(`${process.cwd()}/app/PageObject`);
 
 let pages = {
+    editorPanel: new PageObject('editorPanel.json',stepsPath),
+    moldraw: new PageObject('moldraw.json',stepsPath),
     raptor: new PageObject('raptor.json',stepsPath)
 };
 
