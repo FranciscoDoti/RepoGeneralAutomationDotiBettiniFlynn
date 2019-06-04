d22376266775b59c78721d4ac111a965c3ab74c0
matthew mccurry <matthew.mccurry.contractor@macmillan.com>
2019-06-04 15:33:28 -0500

    added back await

--- app/populate.js
+++ app/populate.js
@@ -57,7 +57,7 @@ const populateSelect = async function (selector, item, WebElementData) {
     await selector.selectByValue(item);
   } else {
     const options = await selector.findElements(By.tagName('option'));
-    for (let option of options)
+    for await (let option of options)
     {
       const optionText = await option.getText();
       if (item === optionText) {
