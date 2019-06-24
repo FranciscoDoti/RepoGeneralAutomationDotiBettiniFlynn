1fd89e90caf5dcc1d580e3b473664c46954ff8f9
Jose Mohamed <jmohamed@sparkdigital.com>
2019-06-24 16:03:09 -0300

    Intermediate SAC test

--- features/ASSESSMENT/steps/sac.js
+++ features/ASSESSMENT/steps/sac.js
@@ -3,12 +3,22 @@ const pages = require(`${process.cwd()}/features/ASSESSMENT/pages/.page.js`).pag
 const driver = require(`${process.cwd()}/app/driver`);
 
 When('I navigate to assignment preview', async function () {
-    await pages.sac.click('assignmentLink');
+    await pages.sac.click('instructorAssignmentLink');
     await pages.sac.click('courseLink');
     await pages.sac.click('assignmentPreviewButton');
+});
+
+When('I navigate to assignment and go back to the course landing page', async function () {
+    await pages.sac.click('studentAssignmentLink');
+    await pages.sac.click('courseLink');
+    await pages.sac.click('breadcrumb2');
 });
 
 Then('The assignment preview is opened in a new tab', async function () {
     await pages.sac.switchToTab('Sapling Learning Student Assignment Container');
     await pages.sac.assertElementExists('previewCheckAnswerButton');
+});
+
+Then('The course landing page is loaded', async function () {
+    await pages.sac.assertElementExists('courseLink');
 });
