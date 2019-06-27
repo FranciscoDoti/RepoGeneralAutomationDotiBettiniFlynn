9adfa2b1cd3eb70fa90f11bbdda7c23c7926fb1b
Thomas Dsilva <thomas.dsilva.contractor@macmillan.com>
2019-06-27 13:57:21 -0500

    modified element namnes

--- features/ASSESSMENT/steps/sac.js
+++ features/ASSESSMENT/steps/sac.js
@@ -3,20 +3,20 @@ const pages = require(`${process.cwd()}/features/ASSESSMENT/pages/.page.js`).pag
 const driver = require(`${process.cwd()}/app/driver`);
 
 When('I navigate to assignment preview', async function () {
-    await pages.sac.click('instructorAssignmentLink');
+    await pages.sac.click('Instructor Assignment Link');
     await pages.sac.click('courseLink');
-    await pages.sac.click('assignmentPreviewButton');
+    await pages.sac.click('Assignment Preview Button');
 });
 
 When('I navigate to assignment and go back to the course landing page', async function () {
-    await pages.sac.click('studentAssignmentLink');
+    await pages.sac.click('Student Assignment Link');
     await pages.sac.click('courseLink');
     await pages.sac.click('breadcrumb2');
 });
 
 Then('The assignment preview is opened in a new tab', async function () {
     await pages.sac.switchToTab('Sapling Learning Student Assignment Container');
-    await pages.sac.assertElementExists('previewCheckAnswerButton');
+    await pages.sac.assertElementExists('Preview Check Answer Button');
 });
 
 Then('The course landing page is loaded', async function () {
