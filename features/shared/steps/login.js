e62bca48ab7e1ec798b09ede5f305824cc419f14
Thomas Dsilva <thomas.dsilva.contractor@macmillan.com>
2019-06-28 19:17:37 +0000

    login.js edited online with Bitbucket

--- features/shared/steps/login.js
+++ features/shared/steps/login.js
@@ -2,7 +2,7 @@ const { Given, When } = require('cucumber');
 const _ = require('lodash');
 const urls = require(`${process.cwd()}/config/urls.json`);
 const pages = require('../pages/.page.js').pages;
-const { visitURL, getDriver } = require(`${process.cwd()}/app/driver`);
+const { visitURL } = require(`${process.cwd()}/app/driver`);
 const users = require(`${process.cwd()}/features/shared/data/users.json`);
 
 /* Verifies Sapling login, AMS page and navigation to AuthorApp page by clicking new Raptor item link */
