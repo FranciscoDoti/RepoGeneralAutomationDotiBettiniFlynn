bdcf1bb98cc338f862225276d76a09fdd2b392d5
kaushikbanerjee <kaushikbanerjee@QAIT.COM>
2019-06-28 22:17:19 +0530

    Completed automation of NGA-7241

--- features/ASSESSMENT/steps/raptor.js
+++ features/ASSESSMENT/steps/raptor.js
@@ -20,3 +20,21 @@ Then(/^I verify item has been created$/, async function () {
     await mathpages.raptorAms.switchToTab('Sapling Learning');
     await pages.raptor.assertElementExists('amsItemCreate', itemid.trim());
 });
+
+Then(/^I verify item has been created with following details$/, async function (dataTable) {
+    let itemid = (await mathpages.ams.getText('getItemid')).split(":")[1].trim();
+    //below two steps need to be added to I add the "(.*)" module
+    await pages.raptor.click('moreButton');
+    await pages.raptor.click('saveAsDraft');
+    await mathpages.raptorAms.switchToTab('Sapling Learning');
+    await getDriver().navigate().refresh();
+    await pages.raptor.assertElementExists('amsItemCreate',itemid.trim());
+    var rows = dataTable.hashes();
+    for (let i = 0; i < dataTable.rows().length; i++) {
+        let field=await rows[i].field;
+        field=field.replace(' ',"_");
+        let value=await rows[i].value;
+        let modulelocator=field+"-"+itemid;
+        await pages.raptor.assertText('raptormoduleType',modulelocator,value);
+    }
+});
