c77af5d17948649998be4f5ab0d28357a5c1460f
Prakhar Kumar Tiwari <prakharkumartiwari@QAIT.COM>
2019-06-29 02:23:33 +0530

    Fixed the changes and taken fresh pull from dev branch

--- features/ASSESSMENT/steps/raptor.js
+++ features/ASSESSMENT/steps/raptor.js
@@ -1,7 +1,9 @@
 
 const { When, Then } = require('cucumber');
 const pages = require(`${process.cwd()}/features/ASSESSMENT/pages/.page`).pages;
-const mathpages = require(`${process.cwd()}/features/MATH/pages/.page.js`).pages;
+const mathpages =require(`${process.cwd()}/features/MATH/pages/.page.js`).pages;
+const { getDriver,sleep } = require(`${process.cwd()}/app/driver`);
+
 
 When(/^I add the "(.*)" module$/, async function (moduleType) {
     await mathpages.ams.click('raptorNewItem');
@@ -9,11 +11,13 @@ When(/^I add the "(.*)" module$/, async function (moduleType) {
     await pages.raptor.click('addLink');
     await pages.raptor.click('modulePallete', moduleType);
     await pages.raptor.click('contentArea');
+    await pages.raptor.populate('chemicalEquationPrefix',"Photosynthesis");
+    await pages.raptor.click('correctContext');
+    await pages.raptor.populate('chemicalEquationAnswerInput',"6 CO2 + 6 H2O → C6H12O6 + 6 O2")
 });
 
-Then(/^I verify item has been created$/, async function () {
-    let itemid = (await mathpages.ams.getText('getItemid')).split(":")[1];
-
+Then(/^I verify item has been created with following details$/, async function (data_table) {
+    let itemid = (await mathpages.ams.getText('getItemid')).split(":")[1].trim();
     //below two steps need to be added to I add the "(.*)" module
     await pages.raptor.click('moreButton');
     await pages.raptor.click('saveAsDraft');
