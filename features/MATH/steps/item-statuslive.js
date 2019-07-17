f419b16c25dd1fd14d95edaf1c540a3042413be0
Vibha Havanur <vibha.havanur.contractor@macmillan.com>
2019-07-17 13:49:27 -0500

    updateItem: remove console,  moved raptor-itemId file to resources, and item step functions single file

--- features/MATH/steps/item-statuslive.js
+++ features/MATH/steps/item-statuslive.js
@@ -1,26 +1,31 @@
-const {When, Then} = require('cucumber');
+const { When, Then } = require('cucumber');
 const pages = require(`${process.cwd()}/features/MATH/pages/.page.js`).pages;
 const expect = require('chai').expect;
+const fs = require('fs');
+
 
 /* Scenario 1: User creates and saves a new AMS raptor item and sets the item status to live */
 
-When(/^I am on the AMS page and click open a saved raptor item$/, async function () {
-  let fs = require('fs');
+Then(/^I note the item Id and save in a temp file$/, async function () {
+  let itemid = await pages.raptorAms.getText('getItemid');
 
+  // writing item id number into a file
+  let num = itemid.split(": ")[1]
+  fs.writeFileSync('features/MATH/resources/raptor-itemId.txt', num);
+});
+
+When(/^I am on the AMS page and click open a saved raptor item$/, async function () {
   // reading item id number from file
-  let savedItemId = fs.readFileSync('raptor-itemId.txt').toString();
+  let savedItemId = fs.readFileSync('features/MATH/resources/raptor-itemId.txt').toString();
+
   await pages.ams.populate('filterSearch', savedItemId.split(' ')[0]);
   await pages.ams.click('itemId', savedItemId.split(' ')[0]);
 });
 
 When(/^I set the item status to live$/, async function () {
   await pages.ams.populate('itemStatus', 'Live');
-  console.log("clicked live");
   await pages.ams.click('itemStatus');
-  console.log("clicked itemStatus");
-
   await pages.ams.click('modalSave');
-  console.log("clicked modalSave");
 });
 
 Then(/^I verify the item reflects status update in "(.*)" element$/, async function (element) {
@@ -28,3 +33,4 @@ Then(/^I verify the item reflects status update in "(.*)" element$/, async funct
   let pageText = await pages.ams.getText(element);
   expect(pageText).to.be.oneOf(['live', 'in progress', 'Algolia is Processing']);
 });
+
