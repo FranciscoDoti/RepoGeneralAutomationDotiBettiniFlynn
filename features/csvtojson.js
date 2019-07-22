0fb771d7d1a2b5e61dd932b7c34de05beaaf97c1
Greeshma Reddy <greeshma.reddy.contractor@macmillan.com>
2019-07-22 11:56:37 -0500

    chenged the path

--- features/csvtojson.js
+++ features/csvtojson.js
@@ -5,7 +5,7 @@ let csvtojson = function (callback) {
   const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   var month = monthNames[today.getMonth()];
 
-  let csvfile = `/Users/greeshma.reddy/downloads/course_report_${month}-${date}-${year}.csv`
+  let csvfile = require(`${process.cwd()}/reports/downloads/course_report_${month}-${date}-${year}.csv`
   const csv = require('csvtojson')
   csv()
     .fromFile(csvfile)
