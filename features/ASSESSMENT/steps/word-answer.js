6d0b5be0a5f26f616c4f1c399cd7d71c94e1bd5b
Jose Mohamed <jmohamed@sparkdigital.com>
2019-07-26 11:57:18 -0300

    Removed unnecessary lines

--- features/ASSESSMENT/steps/word-answer.js
+++ features/ASSESSMENT/steps/word-answer.js
@@ -1,7 +1,6 @@
 const { When, Then } = require('cucumber');
 const pages = require(`${process.cwd()}/features/ASSESSMENT/pages/.page.js`).pages;
 const mathpages = require(`${process.cwd()}/features/MATH/pages/.page.js`).pages;
-const driver = require(`${process.cwd()}/app/driver.js`);
 
 When('I configure the following grading options', async function (datatable) {
   await mathpages.raptorAms.click('correctTab');
@@ -23,12 +22,10 @@ When('I add the following word choices', async function (datatable) {
 Then('I grade the following words', async function (datatable) {
   await pages.raptor.click('More Button');
   await pages.raptor.click('Save As Draft');
-  await driver.sleep(1000);
   await mathpages.raptorAms.click('menuBarMore');
   await pages.raptor.click('checkAnswerSwitchMenu');
 
   for (let i = 0; i < datatable.rows().length; i++) {
-    await pages.wordAnswer.populate('Word Answer Textbox', '');
     await pages.wordAnswer.populate('Word Answer Textbox', datatable.hashes()[i].Word);
     await mathpages.raptorAms.click('checkYourWorkSubmit');
     await pages.raptor.assertText('activeTab', datatable.hashes()[i].Result);
