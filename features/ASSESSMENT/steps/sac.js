a7f7e21636198e091f459d691456694080bbdaab
Jose Mohamed <jmohamed@sparkdigital.com>
2019-08-01 16:38:01 -0300

    SAC - To configure a basic test in SAC

--- features/ASSESSMENT/steps/sac.js
+++ features/ASSESSMENT/steps/sac.js
@@ -5,7 +5,7 @@ const users = require(`${process.cwd()}/features/shared/data/users.json`);
 let scores = [];
 
 When('I navigate to assignment preview', async function () {
-    await pages.sac.click('Instructor Course Link');
+    await pages.sac.click('Course Link');
     await pages.sac.click('Instructor Assessment Link');
     await pages.sac.click('Assignment Preview Button');
     await pages.sac.click('Keep Attempts Button');
@@ -23,7 +23,7 @@ Then('The assignment preview is opened in a new tab', async function () {
 });
 
 When(/^I reset attempts from student "(.*)"$/, async function (userType) {
-    await pages.sac.click('Instructor Course Link');
+    await pages.sac.click('Course Link');
     await pages.sac.click('Instructor Assessment Link');
     await pages.sac.assertElementExists('Student Analysis Tab');
     await pages.sac.click('Student Analysis Tab');
