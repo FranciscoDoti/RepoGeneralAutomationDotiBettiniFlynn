bc8281e8c2b4638e5a1b4e5ae1f3ccd7ae4690e8
Thomas Dsilva <thomas.dsilva.contractor@macmillan.com>
2019-08-27 13:39:35 -0500

    node emailer for Jenkins execution

--- report.js
+++ report.js
@@ -1,20 +1,20 @@
 var reporter = require('cucumber-html-reporter');
- 
+
 var options = {
-        theme: 'bootstrap',
-        jsonFile: './reports/cucumber_report.json',
-        output: './reports/cucumber_report_bs.html',
-        reportSuiteAsScenarios: true,
-        launchReport: true,
-        metadata: {
-            "App Version":"0.3.2",
-            "Test Environment": "STAGING",
-            "Browser": "Chrome  54.0.2840.98",
-            "Platform": "Windows 10",
-            "Parallel": "Scenarios",
-            "Executed": "Remote"
-        }
-    };
- 
-    reporter.generate(options);
-    process.exit();
+    theme: 'bootstrap',
+    jsonFile: `${process.cwd()}/reports/cucumber_report.json`,
+    output: `${process.cwd()}/reports/cucumber_report.html`,
+    reportSuiteAsScenarios: true,
+    launchReport: true,
+    metadata: {
+        "App Version": "0.3.2",
+        "Test Environment": "STAGING",
+        "Browser": "Chrome  54.0.2840.98",
+        "Platform": "Windows 10",
+        "Parallel": "Scenarios",
+        "Executed": "Remote"
+    }
+};
+
+reporter.generate(options);
+process.exit();
--- utils/Email/emailer.js
+++ utils/Email/emailer.js
@@ -1,5 +1,19 @@
 const nodemailer = require('nodemailer');
-const config = require('./emailerconfig.json');
+const argv = require('minimist')(process.argv.slice(2));
+
+const config = {
+    clientId: "701768719333-0f12h7i269l7n2odh5ne80u5mdkth618.apps.googleusercontent.com",
+    clientSecret: "ADEpn82HAOHH9_8lqzpwRIXd",
+    sender: "thomas.dsilva.contractor@macmillan.com",
+    username: "thomas.dsilva.contractor@macmillan.com",
+    refreshToken: "1/3ayBIUgNJWANUL1-rISK50oaD6VlrWuk4XvzG03kzt9rO_ekPBdfvgDHcXLpFiNh",
+    accessToken: "ya29.GltuByf5jWOhzFHv_lbBLQvIahPif5RqTtn_r9TMh7BmFxwaDwn2WJz_zyBGvuVbaGRfElVNYrkqaKDfe9MHic72Mc9W66OeFa-LikboAx0eoZxyq-H1cGT1LbFU1",
+    reportPath: `${process.cwd()}/reports/cucumber_report.html`,
+    environment: argv.env || "int",
+    branch: argv.branch || "development",
+    recepients: argv.recepients || "thomas.dsilva@testingconsultancy.com",
+    result: argv.result || "FAILURE"
+};
 
 let transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -11,15 +25,18 @@ let transporter = nodemailer.createTransport({
 });
 
 let mailOptions = {
-    from    : config.sender, // sender address
-    to      : config.recepients, // list of receivers
-    subject : 'Hello ✔', // Subject line
-    text    : 'Hello world\r\ntest', // plaintext body
-    auth : {
-        user         : config.username,
-        refreshToken : config.refreshToken,
-        accessToken  : config.accessToken,
-        expires      : 1494388182480
+    from: config.sender, // sender address
+    to: config.recepients, // list of receivers
+    subject: `Cucumber pipeline test result - ${config.result}`, // Subject line
+    text: `Please find cucumber report from Jenkins pipeline execution for ${config.branch} branch in ${config.environment} environment attached.`, // plaintext body
+    attachments: [{
+        path: reportPath
+    }],
+    auth: {
+        user: config.username,
+        refreshToken: config.refreshToken,
+        accessToken: config.accessToken,
+        expires: 1494388182480
     }
 };
 
