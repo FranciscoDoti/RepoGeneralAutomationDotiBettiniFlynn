4f38400328a12b8ec84daf9168f06a3b639a6925
Vibha Havanur <vibha.havanur.contractor@macmillan.com>
2019-09-04 17:17:11 -0500

    vbh-MTH-1039: removed chai, updated author equation

--- features/MATH/steps/greq-eval.js
+++ features/MATH/steps/greq-eval.js
@@ -1,5 +1,4 @@
 const { When, Then } = require('cucumber');
-const expect = require('chai').expect;
 const { Key } = require('selenium-webdriver');
 const pages = require(`${process.cwd()}/features/MATH/pages/.page.js`).pages;
 const ngaPages = require(`${process.cwd()}/features/ASSESSMENT/pages/.page.js`).pages;
