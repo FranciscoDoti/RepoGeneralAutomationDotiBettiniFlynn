c94e916ea0d8ed3ab995dc7c62282574d32f94a1
Jose Mohamed <jmohamed@sparkdigital.com>
2019-09-26 19:36:13 -0300

    Taxonomy edition missing

--- features/ASSESSMENT/steps/bulk-update.js
+++ features/ASSESSMENT/steps/bulk-update.js
@@ -16,7 +16,7 @@ When('I add the following draft Raptor items in AMS', async function (datatable)
     await pages.raptor.waitForElementVisibility('Content Area');
     await pages.raptor.click('Content Area');
     let id = (await pages.raptor.getText('Item ID')).split(":")[1].trim();
-    itemIdList[itemIdIndex++] = id;
+    itemIdList[itemIdIndex++] = id + '-' + item.ModuleTypeAcronym;
     await pages.raptor.click('More Menu');
     await pages.raptor.click('More Item Details');
     await pages.raptor.populate('Item Details Title', datatable.hashes()[i].Title);
@@ -30,8 +30,9 @@ When('I add the following draft Raptor items in AMS', async function (datatable)
 
 When('I select the created items in AMS', async function () {
   await pages.ams.switchToTab('Sapling Learning Author Management System');
+  await pages.ams.waitForElementInvisibility('Algolia is Processing');
   for (let i = 0; i < itemIdList.length; i++) {
-    await pages.ams.click('Select Checkbox', itemIdList[i]);
+    await pages.ams.click('Select Checkbox', itemIdList[i].split("-")[0]);
   }
 });
 
@@ -49,39 +50,46 @@ When('I update the selected items with the following details', async function (d
     await pages.update.click('Tree Element Leaf', item.TopicLevel5);
     await pages.update.click('Button', 'Confirm');
 
-    await pages.update.click('Field', 'taxonomy');
-    await pages.update.click('Tree Element', item.TaxonomyLevel1);
-    await pages.update.click('Tree Element Leaf', item.TaxonomyLevel2);
-    await pages.update.click('Button', 'Confirm');
+    // await pages.update.click('Field', 'taxonomy');
+    // await pages.update.click('Tree Element', item.TaxonomyLevel1);
+    // await pages.update.click('Tree Element Leaf', item.TaxonomyLevel2);
+    // await pages.update.click('Button', 'Confirm');
 
     await pages.update.click('Field', 'difficulty');
     await pages.update.click('Option', item.Difficulty);
 
-    await pages.update.click('Field', 'blooms');
+    await pages.update.click('Field', 'status');
     await pages.update.click('Option', item.Status);
 
     await pages.update.click('Field', 'scope');
     await pages.update.click('Option', item.Access);
 
-    await pages.update.click('Save Changes Button');
+    await pages.update.click('Button', 'Save Changes');
   }
   await pages.ams.click('AMS Button', 'Select Action');
   await pages.ams.click('AMS Button', 'Done');
 });
 
 Then('I verify the items were updated in AMS', async function (datatable) {
-
-  // At this point we should wait until Algolia finishes processing
-
-  for (let i = 0; i < datatable.rows().length; i++) {
-    let item = datatable.hashes()[i];
-    await pages.ams.assertText('Author Mode', i, item.AuthorMode);
-    await pages.ams.assertText('Title', i, item.Title);
-    await pages.ams.assertText('Topic', i, item.Topic);
-    await pages.ams.assertText('Taxonomy', i, item.Taxonomy);
-    await pages.ams.assertText('Difficulty', i, item.Difficulty);
-    await pages.ams.assertText('ModuleType', i, item.ModuleType);
-    await pages.ams.assertText('Status', i, item.Status);
-    await pages.ams.assertText('Access', i, item.Access);
+  await pages.ams.waitForElementInvisibility('Algolia is Processing');
+  while (itemIdList.length > 0) {
+    let itemId = itemIdList[0].split('-')[0];
+    let moduleType = itemIdList[0].split('-')[1];
+    let itemTitle = 'NGA QA Test ' + moduleType + ' Item';
+    let item;
+    for (i = 0; i < datatable.rows().length; i++) {
+      if (datatable.hashes()[i].Title == itemTitle) {
+        item = datatable.hashes()[i];
+      }
+    }
+    await pages.ams.assertText('Item Field', 'authoring-tool-' + itemId, item.AuthorMode);
+    await pages.ams.assertText('Item Field', 'title-' + itemId, item.Title);
+    await pages.ams.assertText('Item Field', 'topic-' + itemId, item.Topic);
+    // await pages.ams.assertText('Item Field', 'taxonomy-' + itemId, item.Taxonomy);
+    await pages.ams.assertText('Item Field', 'difficulty-' + itemId, item.Difficulty);
+    await pages.ams.assertText('Item Field', 'status-' + itemId, item.Status);
+    await pages.ams.assertText('Item Field', 'access_type-' + itemId, item.Access);
+    await pages.ams.assertText('Item Field', 'module_type-' + itemId, item.ModuleType);
+    itemIdList.splice(0);
   }
 });
