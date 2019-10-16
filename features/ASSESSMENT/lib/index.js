351e546114a28d1678f87b8f521c767442374c26
Abhishek Tiwari <abhishektiwari@LNLA100P3034.qait.com>
2019-10-16 23:36:40 +0530

    NGA-7874 final changes

--- features/ASSESSMENT/lib/index.js
+++ features/ASSESSMENT/lib/index.js
@@ -2,10 +2,12 @@ const raptorlib = require(`${process.cwd()}/features/ASSESSMENT/lib/raptor.js`);
 const amslib = require(`${process.cwd()}/features/ASSESSMENT/lib/ams.js`);
 const updatelib = require(`${process.cwd()}/features/ASSESSMENT/lib/update.js`);
 const froalalib = require(`${process.cwd()}/features/ASSESSMENT/lib/froala.js`);
+const userlib = require(`${process.cwd()}/features/ASSESSMENT/lib/user.js`);
 
 module.exports = {
     raptorlib,
     amslib,
     updatelib,
-    froalalib
+    froalalib,
+    userlib
 };
---
+++ features/ASSESSMENT/lib/user.js
@@ -0,0 +1,9 @@
+const pages = require(`${process.cwd()}/features/ASSESSMENT/pages/.page.js`).pages;
+
+const searchUser = async function () {
+    
+};
+
+module.exports = {
+    searchUser
+};
