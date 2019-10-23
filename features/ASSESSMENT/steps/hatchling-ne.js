32c60bef9e4a545fe961bc12b56c3e812e4ed83c
Thomas Dsilva <thomas.dsilva.contractor@macmillan.com>
2019-10-23 18:15:47 +0000

    hatchling-ne.js edited online with Bitbucket

--- features/ASSESSMENT/steps/hatchling-ne.js
+++ features/ASSESSMENT/steps/hatchling-ne.js
@@ -3,7 +3,10 @@ const pages = require(`${process.cwd()}/features/ASSESSMENT/pages/.page`).pages;
 const { hatchlinglib } = require(`${process.cwd()}/features/ASSESSMENT/lib/index.js`);
 
 When('I add the following values to Hatchling NE module', async function (datatable) {
-    await hatchlinglib.populateNEValues(datatable);
+    for(let i=0; i<datatable.rows().length; i++){
+        let item = datatable.hashes()[i];
+        await hatchlinglib.populateNEValues(item);
+    }
 });
 
 When('I set the hint for Hatchling NE module with following detail', async function (datatable) {
