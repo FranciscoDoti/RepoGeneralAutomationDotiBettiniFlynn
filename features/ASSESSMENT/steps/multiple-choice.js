44d00cd5ec931e83b1eebb89c87e1fa233d47737
Jose Mohamed <jmohamed@sparkdigital.com>
2019-10-23 16:08:22 -0300

    Used lib

--- features/ASSESSMENT/steps/multiple-choice.js
+++ features/ASSESSMENT/steps/multiple-choice.js
@@ -11,9 +11,7 @@ When(/^I set the number "(.*)" as the correct answwer$/, async function (correct
 
 Then('The variable values are displayed as choices', async function () {
   await pages.raptor.click('Cycle Variables Button');
-  await pages.raptor.click('More Menu');
-  await pages.raptor.click('Save As Draft');
-  await pages.raptor.waitForElementInvisibility('Message', 'Saving');
+  await raptorlib.saveItem();
   let text = await pages.raptor.getText('Choice Text 1');
   switch (text) {
     case "oak":
