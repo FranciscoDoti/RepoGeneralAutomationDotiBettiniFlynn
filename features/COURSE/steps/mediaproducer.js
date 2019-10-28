dceec32bc625c74c67eae9c3a36da7439fed32d4
Greeshma Reddy <greeshma.reddy.contractor@macmillan.com>
2019-10-28 16:11:37 -0500

    WIP

--- features/COURSE/steps/mediaproducer.js
+++ features/COURSE/steps/mediaproducer.js
@@ -19,7 +19,6 @@ When(/^I create Course Template with ISBN "(.*)" and course code "(.*)"$/, async
 });
 
 When(/^I activate the "(.*)" template and add the following data$/, async function (courseName, data_table) {
-  await pages.home.click('closeAlert');
   await pages.courseList.click('courseTemplate', 'COURSE TEMPLATES');
   await pages.courseList.click('courseMenu', courseName);
   await pages.editCourse.click('editCourse');
