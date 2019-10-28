8efd48ee4faeff09b566fc19d1e1f44c2435c55b
Keith Blanchard <keith.blanchard.contractor@macmillan.com>
2019-10-28 17:19:04 -0500

    Adding wait for menu header to load before navigating to the Gradebook. Abstracting course data.

--- features/GRADEBOOK/steps/instructor.js
+++ features/GRADEBOOK/steps/instructor.js
@@ -1,18 +1,20 @@
 const { When, Then, Given } = require('cucumber');
 const pages = require(`${process.cwd()}/features/GRADEBOOK/pages/.page.js`).pages;
-
 const { visitURL } = require(`${process.cwd()}/app/driver`);
 
-Given(/^navigate to my course using course id "(.*)"$/, async function (courseid) {
+Given(/^navigate to my course using course "(.*)"$/, async function (course) {
+  await pages.gradebook.waitForElementVisibility('menuUserName')
+  const courses = this.getCourseData();
   var currentURL = await pages.gradebook.getCurrentURL();
-  var courseURL = currentURL + `/${courseid}/mycourse`;
+  var courseURL = currentURL + `/${courses[course].courseId}/mycourse`;
   await visitURL(courseURL);
 });
 
 When('I click the Gradebook menu link', async function () {
+  await pages.gradebook.waitForElementVisibility('mainNav')
   await pages.gradebook.click('mainNav');
 });
 
 Then('I should see the settings button appear', async function () {
-  await pages.gradebook.assertElementExists('settingsNav');
+  await pages.gradebook.waitForElementVisibility('settingsNav')
 });
--- features/GRADEBOOK/tests/Instructor/viewGradebook.feature
+++ features/GRADEBOOK/tests/Instructor/viewGradebook.feature
@@ -3,6 +3,6 @@ Feature: Admin views the Gradebook
 
   Scenario: Admin views the Gradebook
     Given I login to Achieve-CW as "instructor_8"
-    And navigate to my course using course id "bdvsyb"
+    And navigate to my course using course "course_1"
     When I click the Gradebook menu link
     Then I should see the settings button appear
