fb0733d3458c9a10c1a69d313e2843090b47a0e1
Keith Blanchard <keith.blanchard.contractor@macmillan.com>
2019-11-12 16:48:21 -0600

    Adding points toggle test.

--- features/GRADEBOOK/steps/instructor_given.js
+++ features/GRADEBOOK/steps/instructor_given.js
@@ -1,6 +1,6 @@
 const { getCourseData } = require('../data/courses');
 
-const { When, Then, Given } = require('cucumber');
+const { Given } = require('cucumber');
 const pages = require(`${process.cwd()}/features/GRADEBOOK/pages/.page.js`).pages;
 const { visitURL } = require(`${process.cwd()}/app/driver`);
 
@@ -12,11 +12,7 @@ Given(/^navigate to my course using course "(.*)"$/, async function (course) {
   await visitURL(courseURL);
 });
 
-When('I click the Gradebook menu link', async function () {
-  await pages.gradebook.waitForElementVisibility('mainNav')
+Given('I click the Gradebook menu link', async function () {
+  await pages.gradebook.waitForElementVisibility('mainNav');
   await pages.gradebook.click('mainNav');
-});
-
-Then('I should see the settings button appear', async function () {
-  await pages.gradebook.waitForElementVisibility('settingsNav')
 });
