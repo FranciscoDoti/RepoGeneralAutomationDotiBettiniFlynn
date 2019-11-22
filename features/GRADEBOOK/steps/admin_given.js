d60945e5b331cbaf93de6350cf6e39158286fd92
Scott McPherson <scott_fl@me.com>
2019-11-22 09:59:27 -0500

    Remove old files

--- features/GRADEBOOK/steps/admin_given.js
+++
@@ -1,6 +0,0 @@
-const { Given } = require('cucumber');
-const { sleep } = require(`${process.cwd()}/app/driver`);
-const coursePages = require(`${process.cwd()}/features/COURSE/pages/.page.js`).pages;
-const gradebookPages = require(`${process.cwd()}/features/GRADEBOOK/pages/.page.js`).pages;
-
-
