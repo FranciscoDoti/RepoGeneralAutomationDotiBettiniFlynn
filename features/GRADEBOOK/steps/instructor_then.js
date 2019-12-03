2b32ee25df175a6b8b159486bf486a9a61d96f30
Keith Blanchard <keith.blanchard.contractor@macmillan.com>
2019-12-03 13:26:17 -0600

    Fixing broken test cause by merge conflict.

--- features/GRADEBOOK/steps/instructor_then.js
+++ features/GRADEBOOK/steps/instructor_then.js
@@ -11,20 +11,24 @@ Then('Points are displayed in the course total', async function () {
   const text = await courseTotal[0].getText()
   expect(text).to.equal('50.00\nOut of 100');
 });
+
 Then('Percents are displayed in the course total', async function () {
   await gradebook.waitForElementVisibility('courseTotal');
   const courseTotal = await gradebook.getWebElements('courseTotal');
   const text = await courseTotal[0].getText()
   expect(text).to.equal('50%');
 });
+
 Then('The settings button is visible', async function () {
   await gradebook.waitForElementVisibility('settingsNav');
 });
+
 Then('A new category should appear in the Gradebook', async function () {
   await gradebook.waitForElementVisibility('singleCategory');
   const newCategoryName = getCategoryName();
   await gradebook.assertElementExists('categoryHeaderCells', newCategoryName);
 });
+
 Then('No categories should appear in the Gradebook', async function () {
   await gradebook.waitForElementVisibility('singleCategory');
   const cells = await gradebook.getWebElements('categoryHeaderCells');
@@ -40,32 +44,8 @@ Then('The iclicker menu should be visible', async function () {
   await iclicker.waitForElementVisibility('iClickerMenu');
 });
 
-Then('Points are displayed in the course total', async function () {
-  await gradebook.waitForElementVisibility('courseTotal');
-  const courseTotal = await gradebook.getWebElements('courseTotal');
-  const text = await courseTotal[0].getText()
-  expect(text).to.equal('50.00\nOut of 100');
-});
-Then('Percents are displayed in the course total', async function () {
-  await gradebook.waitForElementVisibility('courseTotal');
-  const courseTotal = await gradebook.getWebElements('courseTotal');
-  const text = await courseTotal[0].getText()
-  expect(text).to.equal('50%');
-});
 Then('The settings button is visible', async function () {
   await gradebook.waitForElementVisibility('settingsNav');
-});
-Then('A new category should appear in the Gradebook', async function () {
-  await gradebook.waitForElementVisibility('singleCategory');
-  const newCategoryName = getCategoryName();
-  await gradebook.assertElementExists('categoryHeaderCells', newCategoryName);
-});
-Then('No categories should appear in the Gradebook', async function () {
-  await gradebook.waitForElementVisibility('singleCategory');
-  const cells = await gradebook.getWebElements('categoryHeaderCells');
-  const text = await cells[0].getText();
-  const newCategoryName = getCategoryName();
-  expect(text).to.contains(newCategoryName);
 });
 
 Then('The sync button should be visible', async function () {
