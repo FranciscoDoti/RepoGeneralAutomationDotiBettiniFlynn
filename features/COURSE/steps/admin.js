90626e847bb7e817e0a1717406d7589ad267d38a
matthew mccurry <matthew.mccurry.contractor@macmillan.com>
2019-12-04 15:52:56 -0600

    Revert "remove click on tab step"
    This reverts commit f75bec8c07fdca17c105d901ea52de723425d655.

--- features/COURSE/steps/admin.js
+++ features/COURSE/steps/admin.js
@@ -98,6 +98,10 @@ Then('I verify the report is dowloaded with following data', async function (dat
   }
 });
 
+When(/^I click on "(.*)" Tab$/, async function (tabName){
+  await pages.coursePage.click('navigation', tabName);
+});
+
 Then('I verify that following Tab are present', async function (data_table){
   for (let i = 0; i < data_table.rows().length; i++) {
     await pages.coursePage.assertElementExists('Tab', data_table.hashes()[i].Tabs)
