41ae94ae25ec924b425ba981357ac5e0561d6c64
Greeshma Reddy <greeshma.reddy.contractor@macmillan.com>
2019-12-06 09:55:04 -0500

    Removed double Click on the search button

--- features/COURSE/steps/mediaproducer.js
+++ features/COURSE/steps/mediaproducer.js
@@ -19,8 +19,6 @@ When(/^I create Course Template with ISBN "(.*)" and course code "(.*)"$/, async
 });
 
 When(/^I activate the "(.*)" template and add the following data$/, async function (courseName, data_table) {
-  await pages.courseList.populate('search', courseName);
-  await sleep(500);
   await pages.courseList.click('courseMenu', courseName);
   await pages.editCourse.click('editCourse');
   for (let i = 0; i < data_table.rows().length; i++) {
