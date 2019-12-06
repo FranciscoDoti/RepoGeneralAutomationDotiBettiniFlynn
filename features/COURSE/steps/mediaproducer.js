fde514f6e69d8d96f9a87337d15930cd3f83eb42
Greeshma Reddy <greeshma.reddy.contractor@macmillan.com>
2019-12-06 10:00:38 -0500

    merge conflict

--- features/COURSE/steps/mediaproducer.js
+++ features/COURSE/steps/mediaproducer.js
@@ -19,12 +19,7 @@ When(/^I create Course Template with ISBN "(.*)" and course code "(.*)"$/, async
 });
 
 When(/^I activate the "(.*)" template and add the following data$/, async function (courseName, data_table) {
-<<<<<<< HEAD
   await sleep(500);
-=======
-  await pages.courseList.populate('search', courseName);
-  await pages.courseList.waitForElementVisibility('courseMenu', courseName);
->>>>>>> 9d3068002a90b4d3901e16892c2ad56c3695a59b
   await pages.courseList.click('courseMenu', courseName);
   await pages.editCourse.click('editCourse');
   for (let i = 0; i < data_table.rows().length; i++) {
