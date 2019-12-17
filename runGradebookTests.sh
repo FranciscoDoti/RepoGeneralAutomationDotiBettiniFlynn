2c4d0394f6602e20d318b64e0ebd03e681641bbe
Keith Blanchard <keith.blanchard.contractor@macmillan.com>
2019-12-17 11:14:49 -0600

    Removing rerun script.

--- runGradebookTests.sh
+++
@@ -1,8 +0,0 @@
-node ./node_modules/cucumber/bin/cucumber-js -f rerun --format rerun:@rerun1.txt --tags '@Gradebook' ;
-
-
-#node ./node_modules/cucumber/bin/cucumber-js @rerun1.txt -f rerun --format rerun:@rerun2.txt ;
-
-node ./node_modules/cucumber/bin/cucumber-js @rerun1.txt -f json:reports/cucumber_report.json ;
-
-rm @rerun*
