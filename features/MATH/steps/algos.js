7321042be83955a9509a10f497c8204e80b15cec
Vibha Havanur <vibha.havanur.contractor@macmillan.com>
2020-03-06 08:11:10 -0600

    vbh-MTH: renamed algos object  amd updated element xpath

--- features/MATH/steps/algos.js
+++ features/MATH/steps/algos.js
@@ -9,24 +9,24 @@ When(/^I select Question tab and click on algos Variables button$/, async functi
 
 When(/^I click on List Variable button, list algos table is added$/, async function () {
     await pages.raptorAlgos.click('algosButton', '+ List Variable');
-    await pages.raptorAlgos.assertElementExists('listAlgosVariableName');
+    await pages.raptorAlgos.assertElementExists('variableName1ListAlgos');
   });
 
   When(/^I click on Add Column and Delete Column buttons, list column is added and deleted respectively$/, async function () {
     await pages.raptorAlgos.click('algosButton', 'Add Column');
-    await pages.raptorAlgos.assertElementExists('listAlgosInput');
+    await pages.raptorAlgos.assertElementExists('row1Column2ListAlgos');
     await pages.raptorAlgos.click('algosButton', 'Value 2');
-    await pages.raptorAlgos.assertElementDoesNotExist('listAlgosInput');
+    await pages.raptorAlgos.assertElementDoesNotExist('row1Column2ListAlgos');
   });
 
   When(/^I click on Add Row and Delete Row buttons, list row is added and deleted respectively$/, async function () {
     await pages.raptorAlgos.click('algosButton', 'Add Row');
-    await pages.raptorAlgos.assertElementExists('rowAlgosInput');
-    await pages.raptorAlgos.click('rowAlgosDelete');
-    await pages.raptorAlgos.assertElementDoesNotExist('rowAlgosInput');
+    await pages.raptorAlgos.assertElementExists('row2ListAlgos');
+    await pages.raptorAlgos.click('row2ListAlgosDelete');
+    await pages.raptorAlgos.assertElementDoesNotExist('row2ListAlgos');
   });
 
   When(/^I click on Remove Table button, list algos table is deleted$/, async function () {
     await pages.raptorAlgos.click('algosButton', 'Remove Table');
-    await pages.raptorAlgos.assertElementDoesNotExist('listAlgosVariableName');
+    await pages.raptorAlgos.assertElementDoesNotExist('variableName1ListAlgos');
   });
