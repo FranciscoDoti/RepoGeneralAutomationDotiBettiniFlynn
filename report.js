ac29efdbc192123b7f6936c930b8a72bd468e79b
Thomas Dsilva <thomas.dsilva.contractor@macmillan.com>
2020-04-20 16:56:57 +0530

    Merged lint into development

--- report.js
+++ report.js
@@ -1,21 +1,21 @@
-var reporter = require('cucumber-html-reporter');
+const reporter = require('cucumber-html-reporter');
 const argv = require('minimist')(process.argv.slice(2));
 const jsonfile = require('jsonfile');
 
-const reportName = function(){
-    var prefix = argv.prefix || "";
-    prefix = (prefix !== "") ? prefix + " " : "";
-    var date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-    return prefix + "Cucumber Report " + date;
+const reportName = () => {
+  let prefix = argv.prefix || '';
+  prefix = (prefix !== '') ? `${prefix} ` : '';
+  const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+  return `${prefix}Cucumber Report ${date}`;
 };
 
-var options = {
-    theme: 'bootstrap',
-    jsonFile: `${process.cwd()}/reports/cucumber_report.json`,
-    output: `${process.cwd()}/reports/${reportName()}.html`,
-    reportSuiteAsScenarios: true,
-    launchReport: true,
-    metadata: jsonfile.readFileSync(`${process.cwd()}/reports/cucumber_report.json`)[0].metadata
+const options = {
+  theme: 'bootstrap',
+  jsonFile: `${process.cwd()}/reports/cucumber_report.json`,
+  output: `${process.cwd()}/reports/${reportName()}.html`,
+  reportSuiteAsScenarios: true,
+  launchReport: true,
+  metadata: jsonfile.readFileSync(`${process.cwd()}/reports/cucumber_report.json`)[0].metadata,
 };
 
 reporter.generate(options);
