b84d2e9edc8dc1d410d850686582de7b954b751d
Thomas Dsilva <thomas.dsilva.contractor@macmillan.com>
2020-04-25 18:29:07 +0000

    Merged coursefixes into master
    * added fixes for rest
    
    * updated repo rc with correct params
    
    * package version update

--- features/COURSE/steps/courseapi.js
+++ features/COURSE/steps/courseapi.js
@@ -5,20 +5,20 @@ const specPath = `${process.cwd()}/features/COURSE/apispecs`;
 
 When('I create a course as {string} with the following data', async function (userType, datatable) {
   let spec = `${specPath}/createcourse.json`;
-  let jwt_payload = this.users[userType].jwt_payload;
+  let jwt = this.users[userType].jwt_payload;
   let api = new RestObject(spec);
-  await api.setCookie(jwt_payload);
 
   for (let data of datatable.hashes()) {
     let randomNumber = Math.floor(Math.random() * 10000000000000)
     data.isbn = '"' + randomNumber + '"'
-    data.owner_id = jwt_payload.user_id;
-    expect(await api.POST(this.apiserver, data)).to.equal(200);
+    data.owner_id = jwt.user_id;
+    const response = await api.post(this.apiserver, jwt, data);
+    expect((response.statusCode || response.status)).to.equal(200);
     this.data.set(data.name, {
       courseName: data.name,
       short_name: data.short_name,
       isbn: data.isbn,
-      id: api.response.id,
+      id: response.id,
     });
   }
 });
@@ -26,11 +26,9 @@ When('I create a course as {string} with the following data', async function (us
 When('I copy course from {string} as {string} with the following data', async function (courseName, userType, datatable) {
   let courseId = this.data.get(courseName).id;
   let spec = `${specPath}/copycourse.json`;
-  let jwt_payload = this.users[userType].jwt_payload;
+  let jwt = this.users[userType].jwt_payload;
   let api = new RestObject(spec);
-  api.setCookie(jwt_payload);
-  api.spec.endpoint = api.spec.endpoint.replace('{id}', courseId);
-
+  
   for (let data of datatable.hashes()) {
     const current = new Date();
     let format  = current. toISOString().slice(0,10)
@@ -38,39 +36,42 @@ When('I copy course from {string} as {string} with the following data', async fu
     format_Next =  next_date.toISOString().slice(0,10)
     data.enrollment_start_date = format;
     data.course_end_date = format_Next;
-    await api.POST(this.apiserver, data)
+    
+    await api.setResource((await api.getResource()).replace('ReplaceID',courseId));
+    const response = await api.put(this.apiserver, jwt, data);
+    console.log(response);
+
     this.data.set(data.name, {
       courseName: data.name,
       short_name: data.short_name,
       isbn: data.isbn,
-      id: api.response.id,
+      id: response.id,
     });
-    console.log(api.response.courseId);
   }
 });
 
 When('I assign instructor to {string} as a {string}', async function(courseName, userName, datatable) {
   let courseId = this.data.get(courseName).id;
   let spec = `${specPath}/enrollInstructor.json`;
-  let jwt_payload = this.users[userName].jwt_payload;
+  let jwt = this.users[userName].jwt_payload;
   let api = new RestObject(spec);
-  api.setCookie(jwt_payload);
-  api.spec.endpoint = api.spec.endpoint.replace('{id}', courseId);
+
   for (let data of datatable.hashes()) {
     data.enrollments = this.users[data.enrollments].jwt_payload;
-    data.id = courseId
-    await api.PUT(this.apiserver, data)
+    data.id = courseId;
+
+    await api.setResource((await api.getResource()).replace('ReplaceID',courseId));
+    const response = await api.put(this.apiserver, jwt, data);
+    console.log(response);
   }
 });
 
 When('I activate {string} as {string} with following data', async function (courseName, userType, datatable) {
   let courseId = this.data.get(courseName).id;
   let spec = `${specPath}/activate.json`;
-  let jwt_payload = this.users[userType].jwt_payload.instructors;
+  let jwt = this.users[userType].jwt_payload.instructors;
   let api = new RestObject(spec);
-  api.setCookie(jwt_payload);
-  api.spec.endpoint = api.spec.endpoint.replace('{id}', courseId);
-
+  
   const date = new Date();
   const futureDate = new Date();
   futureDate.setMonth(futureDate.getMonth()+3);
@@ -83,43 +84,48 @@ When('I activate {string} as {string} with following data', async function (cour
     data.enrollment_start_date = DateJson;
     data.course_end_date = futureJson;
     data.enrollments = user;
-    await api.PUT(this.apiserver, data)
-    console.log(api.response);
+
+    await api.setResource((await api.getResource()).replace('ReplaceID',courseId));
+    const response = await api.put(this.apiserver, jwt, data);
+    console.log(response);
   }
 });
 
 When('I add activities to the content library of {string} template', async function (courseName, datatable) {
     let courseId = this.data.get(courseName, 'id');
     let spec = `${specPath}/addingActivities.json`;
-    let jwt_payload = this.users['admin_1'].jwt_payload;
+    let jwt = this.users['admin_1'].jwt_payload;
     let api = new RestObject(spec);
-    api.setCookie(jwt_payload);
-    api.spec.endpoint = api.spec.endpoint.replace('{id}', courseId.id);
+
     let data = {}
     let items = []
-    
     for (let row of datatable.hashes()) {
-      let item = await activitySearch(row.name, jwt_payload)
+      let item = await activitySearch(row.name, jwt, this)
       await items.push(item)
     }
     
-    data.items = items
-    expect(await api.PUT(this.apiserver, data)).to.equal(200)
+    data.items = items;
+    await api.setResource((await api.getResource()).replace('ReplaceID',courseId));
+    const response = await api.put(this.apiserver, jwt, data);
+    console.log(response);
+    expect((response.statusCode || response.status)).to.equal(200)
     
 })
 
-async function activitySearch(activityName, jwt_payload){
+async function activitySearch(activityName, jwt, context){
   let spec = `${specPath}/searchActivity.json`;
   let api = new RestObject(spec);
-  api.setCookie(jwt_payload);
+
   let data = {}
-  data.searchTerm = activityName
-  await api.POST(this.apiserver, data)
+  data.searchTerm = activityName;
+  const response = await api.put(context.apiserver, jwt, data);
+  console.log(response);
+
   let item = {}
-  item.name = await api.response.hits[0].name
-  item.objectId = await api.response.hits[0].objectID
-  item.tool = await api.response.hits[0].type
-  item.activity_id = await api.response.hits[0].activity_id
+  item.name = await response.hits[0].name
+  item.objectId = await response.hits[0].objectID
+  item.tool = await response.hits[0].type
+  item.activity_id = await response.hits[0].activity_id
   item.isInContentLibrary = false
   item.isDisabled = false
   item.actionText = "Show in Content Library"
--- features/COURSE/tests/Admin/Qualitative/URL.feature
+++ features/COURSE/tests/Admin/Qualitative/URL.feature
@@ -1,7 +1,7 @@
 @Course @Smoke @API
 Feature: Verify that Admin is able to create URL
 
-   @delete-Courses @WIP
+   @delete-Courses
     Scenario: Verify that Admin is able to create a custom tsak with URL in Qual course
 
        Given I login to Achieve-CW as "admin_1"
@@ -10,25 +10,25 @@ Feature: Verify that Admin is able to create URL
             | Qualitative URL Template            | E2E 301    | topics | draft  | 4                | true               | 0053B000001YyTMQA0  | template      | 57ba5934-30c2-4558-b776-b4bef6954d99    |  false        |  9781464199499   | 1                 |
                             
 
-        # And I click on search button and input "Qualitative URL Template" to search the course
+        And I click on search button and input "Qualitative URL Template" to search the course
 
-        # And I click on "Qualitative URL Template" card
-        # And I click on "Production" Tab
+        And I click on "Qualitative URL Template" card
+        And I click on "Production" Tab
 
-        # And I add URL link to "Create"
-        #     | field           | link                   |
-        #     | addUrlLinkinput | https://www.google.com |
+        And I add URL link to "Create"
+            | field           | link                   |
+            | addUrlLinkinput | https://www.google.com |
 
-        # Then I verify that activties are added in "Create"
-        #     | activity |
-        #     | Google   |
+        Then I verify that activties are added in "Create"
+            | activity |
+            | Google   |
 
-        # And I add custom activity to Content Library
-        #     | activity |
-        #     | Google   |
+        And I add custom activity to Content Library
+            | activity |
+            | Google   |
 
-        # Then I verify that activties are added in "Content Library"
-        #     | activity |
-        #     | Google   |
+        Then I verify that activties are added in "Content Library"
+            | activity |
+            | Google   |
 
 
